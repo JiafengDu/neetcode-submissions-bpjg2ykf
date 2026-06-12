2b57fc5abbed54be9043d2782b5a178ff290d7e7
DaveTheGiant <49822677+JiafengDu@users.noreply.github.com>
2026-06-11 17:14:23 -0700

    Add: sliding-window-maximum - submission-2

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-2.py
@@ -0,0 +1,17 @@
+import collections
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        q = collections.deque()
+        
+        for i, num in enumerate(nums):
+            if q and q[0] < i-k+1:
+                q.popleft()
+            while q and nums[q[-1]] < num:
+                q.pop()
+            
+            q.append(i)
+            if i>=k-1:
+                res.append(nums[q[0]])
+
+        return res
